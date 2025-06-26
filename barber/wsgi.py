f0d64a82c6f8d57d9ac54dbd9410c01115d883d3
Daniel Vanegas <vanegasocampo@gmail.com>
2025-06-26 11:56:09 -0500

    Corregir render.yaml para forzar migraciones y crear superuser

--- barber/wsgi.py
+++ barber/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from django.contrib.auth import get_user_model
+import django
+from django.core.management import call_command
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'barber.settings')
 
@@ -18,5 +21,10 @@ application = get_wsgi_application()
 
 if os.environ.get('RENDER_CREATE_SUPERUSER') == 'true':
     User = get_user_model()
-    if not User.objects.filter(username='admin').exists():
-        User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
+    username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+    email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
+    password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'admin123')
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username, email, password)
+
+
